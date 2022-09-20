83bb6447e8b124a633198b57cab5673b8c09bad3
franc1899 <francisco.ordenesv@uc.cl>
2022-09-20 15:37:12 -0300

    logout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import { Auth } from "aws-amplify";
+import { useUser } from "../context/AuthContext";
 
 export default function Home() {
   const router = useRouter();
+  const { user } = useUser();
+  console.log(user);
+
+  async function signOut() {
+    try {
+        await Auth.signOut();
+        router.push("/");
+      } catch (error) {
+          console.log('error signing out: ', error);
+      }
+  }
 
   return (
     <div>
@@ -9,6 +22,8 @@ export default function Home() {
     <button onClick={() => router.push("/todo")}> Go to ToDo list </button>
     <button onClick={() => router.push("/signup")}> Go to Signup </button>
     <button onClick={() => router.push("/login")}> Go to Login </button>
+    <button onClick={signOut}> Logout </button>
+
     </div>
   )
 }
